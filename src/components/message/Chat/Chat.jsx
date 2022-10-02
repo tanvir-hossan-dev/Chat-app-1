7c644ffee3eb70922f897bd929ed76a69dc69d6c
Tanvir Hossan <tanvirhossan27@gmail.com>
2022-10-02 22:26:04 +0600

    message page code refector

--- src/components/message/Chat/Chat.jsx
+++ src/components/message/Chat/Chat.jsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import ChatButton from "./ChatButton";
 import ChatHeader from "./ChatHeader";
 import ChatMessage from "./ChatMessage";
+import { getDatabase, ref, onValue } from "firebase/database";
+import { getUsers } from "../../../redux/features/users/usersSlice";
 
 const Chat = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const db = getDatabase();
+    const starCountRef = ref(db, "users/");
+    onValue(starCountRef, (snapshot) => {
+      const data = snapshot.val();
+      const Arr = data && Object.keys(data).map((key) => ({ uid: key, ...data[key] }));
+
+      dispatch(getUsers(Arr));
+    });
+  }, [dispatch]);
   return (
     <div className="px-8 py-4 shadow-md rounded-md min-h-screen">
       <ChatHeader />
